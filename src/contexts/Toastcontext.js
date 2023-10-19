3e00231ebe123a8d64903d66eb25169ea2aa4c8e
Raafatbaki <raafat.baki.93@gmail.com>
2023-10-19 18:35:15 +0200

    commit

--- src/contexts/Toastcontext.js
+++ src/contexts/Toastcontext.js
@@ -3,13 +3,13 @@ import MySnackBar from "../components/MySnackBar";
 
 export const TosatContext = createContext({});
 
-// export const TosatPrevoider = ({ children }) => {
-//   return (
-//     <>
-//       <TosatPrevoider>
-//         <MySnackBar></MySnackBar>
-//         {children}
-//       </TosatPrevoider>
-//     </>
-//   );
-// };
+export const TosatPrevoider = ({ children }) => {
+  return (
+    <>
+      <TosatPrevoider>
+        <MySnackBar></MySnackBar>
+        {children}
+      </TosatPrevoider>
+    </>
+  );
+};
